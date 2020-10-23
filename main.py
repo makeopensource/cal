9f8623e6fc9ee3352cd069df2e222421dac53288
Emil Kovacev <emilkovacev@gmail.com>
2020-10-23 18:12:45 -0400

    added argparser functionality for projects

--- main.py
+++ main.py
@@ -2,19 +2,26 @@ import argparse
 import sys
 import os
 
-from project import make_project
+from project import make_project as mp
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("project", action = "store_true")
-    parser.add_argument("dir_name", action = "store_true")
+    parser.add_argument("argA")
+    parser.add_argument("--projectname", action="store_true")
 
     args = parser.parse_args()
 
-    currdir = os.getcwd() # current directory
-
     # creates a project, assigns project to directory if PATH defined.
-    if (args.project):
-        make_project(args.dir_name)
+    if (args.argA == "project"):
+        print("creating project...")
+        mp.make_project()
+        sys.exit(0)
 
+    elif args.project and args.projectname:
+        print("creating project...")
+        mp.make_project(args.projectname)
+        sys.exit(0)
+
+    else:
+        print("project creation failed")
