13f27f8efc3d640a51d717bbec3104d4d61fdf8c
adam3 <adam38999@gmail.com>
2020-10-24 17:22:27 -0400

    Added restore, added safeties to improper usage

--- freezerestore/freeze.py
+++ freezerestore/freeze.py
@@ -1,3 +1,8 @@
 import shutil
+import os.path
+from os import path
+
 def freeze(filetitle):
+    if not path.exists(filetitle):
+        return
     shutil.copy(filetitle,filetitle + ".temp")
---
+++ freezerestore/restore.py
@@ -0,0 +1,12 @@
+import shutil
+import os.path
+from os import path
+
+def freeze(filetitle):
+    if not path.exists(filetitle):
+        return
+    if not path.exists(filetitle + ".temp"):
+        return
+    shutil.move(filetitle, filetitle + ".temp2")
+    shutil.move(filetitle + ".temp", filetitle)
+    shutil.move(filetitle + ".temp2", filetitle + ".temp2")
