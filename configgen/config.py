5774a8d30a1c90883dda487e8ea7650f97c7454b
adam russell <adam38999@gmail.com>
2020-10-24 19:11:05 -0400

    Removed obsolete code

--- configgen/config.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-from os.path import expanduser
-
-def config():
-    home = expanduser("~")
-    config = {"location": home + "/.calfreeze"}
-    with open("~/.config/cal/calconfig.json", "w") as f:
-        json.dump(config, f)
