17dc32eeac94e55b6449d61eb0c7e71f5f1c1747
adam russell <adam38999@gmail.com>
2020-10-24 19:36:27 -0400

    fixed check.py

--- project/check.py
+++ project/check.py
@@ -3,19 +3,6 @@ import sys
 import os
 
 
-def mkprj(path=os.getcwd()):
-    if path != os.getcwd():
-        os.mkdir(path)
-        return path
-    else:
-        separator = "\\"  # path separator (change for linux)
-        return path.split(separator)[-1]
-
-
-print(mkprj())
-
-
-
 def listDirectory(dictionary):
     print("Projects:")
     # This is mostly for formating purposes
