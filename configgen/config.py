5159f74cfe41bd71d3bbb891a0f508eafac29c20
adam3 <adam38999@gmail.com>
2020-10-24 17:11:37 -0400

    Fixed an error in config.py

--- configgen/config.py
+++ configgen/config.py
@@ -1,5 +1,9 @@
 import json
+import os
+from os.path import expanduser
+
 def config():
-    config = {"location": "~/.calfreeze"}
+    home = expanduser("~")
+    config = {"location": home + "/.calfreeze"}
     with open("~/.config/cal/calconfig.json", "w") as f:
         json.dump(config, f)
