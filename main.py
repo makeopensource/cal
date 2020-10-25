4161fb0705edbb599a8ee4687443fcf0c48655ce
Nick Brown <bicknrown@gmail.com>
2020-10-24 22:33:09 -0400

    added command functions to main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import os
+import project as p
 
 from project import make_project as mp
 
@@ -12,13 +13,36 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if operation == "project":
-        print("make a project (use code from github)")
-    elif operation == "check":
-        print("use check (needs projectname)")
-    elif operation == "freeze":
-        print("run freeze (needs filename)")
-    elif operation == "restore":
-        print("run restore (also needs filename)")
+    if arg.operation == "project":
+        if arg.argname == "":
+            print("Creating project...")
+            p.make_project()
+            sys.exit(0)
+        elif:
+            print("Creating project...")
+            p.make_project(arg.argname)
+            sys.exit(0)
+    elif arg.operation == "check":
+        if arg.argname == "":
+            print("You didn't give a project name!")
+        elif:
+            print("Checking project status...")
+            p.check(arg.argname)
+            sys.exit(0)
+    elif arg.operation == "freeze":
+        if arg.argname == "":
+            print("You didn't input a file name!")
+        elif:
+            print("Freezing file " + arg.argname + ".")
+            p.freeze(arg.argname)
+            sys.exit(0)
+    elif arg.operation == "restore":
+        if arg.argname == "":
+            print("You didn't input a file name!")
+        elif:
+            print("Restoring file " + arg.argname + ".")
+            p.restore(arg.argname)
+            sys.exit(0)
+
     else:
         print("Command not found.")
