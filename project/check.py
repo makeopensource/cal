4deb427b55bcea03ed848d1efcd50806eb9d143c
jws24 <jakesavi@buffalo.edu>
2020-10-24 23:21:32 -0400

    YA BOY CHECK AT IT AGAIN

--- project/check.py
+++ project/check.py
@@ -2,13 +2,13 @@
 import sys
 import os
 import json
-def listDirectory():
+def check():
     print("Projects:")
     if os.path.isfile("metadata.json"):
         with open("metadata.json", "r") as f:
             meta = json.load(f)
     else:
         meta = {}
-    # This is mostly for formating purposes
+    # This is mostly for formatting purposes
     for keys in meta.keys():
         print(meta.get(keys, ""))
