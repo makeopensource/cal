6ddb3e3b8682b9007f0fb8c407839ed059c87d4d
Nick Brown <bicknrown@gmail.com>
2020-10-25 09:41:10 -0400

    put main inside a function

--- main.py
+++ main.py
@@ -7,44 +7,44 @@ from project import make_project as p
 from project.freezerestore import freeze as f
 from project.freezerestore import restore as r
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("operation", action="store", type=str)
-    parser.add_argument("--argname", default="", action="store", type=str)
+def main():
+    if __name__ == "__main__":
+        parser = argparse.ArgumentParser()
+        parser.add_argument("operation", action="store", type=str)
+        parser.add_argument("--argname", default="", action="store", type=str)
+        arg = parser.parse_args()
 
-    arg = parser.parse_args()
+        if arg.operation == "project":
+            if arg.argname == "":
+                print("Creating project...")
+                p.make_project()
+                sys.exit(0)
+            else:
+                print("Creating project...")
+                p.make_project(arg.argname)
+                sys.exit(0)
+        elif arg.operation == "check":
+            print("Checking project status...")
+            c.check()
+            sys.exit(0)
+        elif arg.operation == "submit":
+            print("Opening submit link...")
+            s.submit("")
+            sys.exit(0)
+        elif arg.operation == "freeze":
+            if arg.argname == "":
+                print("You didn't input a file name!")
+            else:
+                print("Freezing file " + arg.argname + ".")
+                f.freeze(arg.argname)
+                sys.exit(0)
+        elif arg.operation == "restore":
+            if arg.argname == "":
+                print("You didn't input a file name!")
+            else:
+                print("Restoring file " + arg.argname + ".")
+                r.restore(arg.argname)
+                sys.exit(0)
 
-    if arg.operation == "project":
-        if arg.argname == "":
-            print("Creating project...")
-            p.make_project()
-            sys.exit(0)
         else:
-            print("Creating project...")
-            p.make_project(arg.argname)
-            sys.exit(0)
-    elif arg.operation == "check":
-        print("Checking project status...")
-        c.check()
-        sys.exit(0)
-    elif arg.operation == "submit":
-        print("Opening submit link...")
-        s.submit("")
-        sys.exit(0)
-    elif arg.operation == "freeze":
-        if arg.argname == "":
-            print("You didn't input a file name!")
-        else:
-            print("Freezing file " + arg.argname + ".")
-            f.freeze(arg.argname)
-            sys.exit(0)
-    elif arg.operation == "restore":
-        if arg.argname == "":
-            print("You didn't input a file name!")
-        else:
-            print("Restoring file " + arg.argname + ".")
-            r.restore(arg.argname)
-            sys.exit(0)
-
-    else:
-        print("Command not found.")
+            print("Command not found.")
